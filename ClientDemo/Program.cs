50a73cdd0385713170ef93d28db6232a847053bb
hph <admin@hepeihua.com>
2019-08-26 15:29:08 +0800

    first commit

---
+++ ClientDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+
+namespace ClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            //不带账户密码去请求认证服务器
+            //var  noAuthToGet = client.GetDiscoveryDocumentAsync("http://localhost:5000").Result;
+
+            //if(noAuthToGet.IsError)
+            //{
+            //    Console.WriteLine("No Auth To Get Status Code = " + noAuthToGet.Error);
+            //}
+             
+            // 带上账户去请求,获取access token
+            var tokenResponse = client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = "http://localhost:5000/connect/token",
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            }).Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);                
+            }
+            Console.WriteLine(tokenResponse.Json);
+
+            //没有设置token的时候访问api
+            string apiUrl = "http://localhost:6000/api/values";
+            var withoutTokenResp = client.GetAsync(apiUrl).Result;
+            if(withoutTokenResp.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine("withoutToken  Response Status Code = " + withoutTokenResp.StatusCode );
+            }
+
+            //将得到的access token 请求api
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var apiRes = client.GetStringAsync(apiUrl).Result;
+
+            Console.WriteLine("Request http://localhost:6000/api/values ");
+            Console.WriteLine(apiRes);
+
+            //通过 postman 验证 token
+            //在HttpHeader增加Authorization值为Bearer AccessToken的值 
+            //例如Authorization:BearereyJhbGciOiJSUzI1NiIsImtpZCI6ImI0ZjEyMzI5YWJkZDlmYzIzZTU4MmJmM2ZkNjVlOTRkIiwidHlwIjoiSldUIn0
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IdentityServer4Demo/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer4Demo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                    .AddDeveloperSigningCredential()  //开发环境中使用,每次启动时,为令牌签名创建了一个临时密钥.在生成环境需要一个持久化的密钥
+                    .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                    .AddInMemoryApiResources(Config.GetApis())
+                    .AddInMemoryClients(Config.GetClients());
+             
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            } 
+            app.UseIdentityServer();
+        }
+    }
+}
